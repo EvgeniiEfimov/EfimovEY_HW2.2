e67fb0fc15752fd006bcc10500b296382d798a3b
1.0.0 <1.0.0>
2021-07-09 18:43:46 +0600

    Initial Commit

---
+++ EfimovEY_HW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EfimovEY_HW2.2
+//
+//  Created by User on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
