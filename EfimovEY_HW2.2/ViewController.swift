e36b7bde39cd6085fa57c13c7e13c08082807a48
1.0.0 <1.0.0>
2021-07-11 13:21:48 +0300

    HW 2.2, storyboard

--- EfimovEY_HW2.2/ViewController.swift
+++ EfimovEY_HW2.2/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorViewOutlet: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
