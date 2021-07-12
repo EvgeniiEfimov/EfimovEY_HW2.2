b4404f3bb0e065af3775ca44ef83bc122649c268
1.0.0 <1.0.0>
2021-07-12 16:50:55 +0300

    100%

--- EfimovEY_HW2.2/ViewController.swift
+++ EfimovEY_HW2.2/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum Color {
+    case Red, Green, Blue
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorViewOutlet: UIView!
@@ -19,28 +23,39 @@ class ViewController: UIViewController {
     
     @IBOutlet var valueColor: [UILabel]!
     
-    enum Color {
-        case Red, Green, Blue
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         valueColor[0].textColor = UIColor.red
-        valueColor[0].text = "\(CGFloat(sliderOutlet[0].value))"
         valueColor[1].textColor = UIColor.green
-        valueColor[1].text = "\(CGFloat(sliderOutlet[1].value))"
         valueColor[2].textColor = UIColor.blue
+        valueColor[0].text = "\(CGFloat(sliderOutlet[0].value))"
+        valueColor[1].text = "\(CGFloat(sliderOutlet[1].value))"
         valueColor[2].text = "\(CGFloat(sliderOutlet[2].value))"
         
         redColorLabel.textColor = UIColor.red
-        redColorLabel.text = "\(Color.Red):"
         greenColorLabel.textColor = UIColor.green
-        greenColorLabel.text = "\(Color.Green):"
         blueColorLabel.textColor = UIColor.blue
+        redColorLabel.text = "\(Color.Red):"
+        greenColorLabel.text = "\(Color.Green):"
         blueColorLabel.text = "\(Color.Blue):"
-        
+        setingSlider()
+    }
+    
+    @IBAction func allSliderAction(_ sender: UISlider) {
+        viewBackgroundColor()
+        switch sender.tag {
+        case 1:
+            valueColorText(slider: 0)
+        case 2:
+            valueColorText(slider: 1)
+        default:
+            valueColorText(slider: 2)
+        }
+    }
+    
+    private func setingSlider() {
         sliderOutlet[0].minimumTrackTintColor = UIColor.red
         sliderOutlet[1].minimumTrackTintColor = UIColor.green
         sliderOutlet[2].minimumTrackTintColor = UIColor.blue
@@ -50,24 +65,14 @@ class ViewController: UIViewController {
     }
     
     private func viewBackgroundColor(){
-        colorViewOutlet.backgroundColor = UIColor.init(red: CGFloat(sliderOutlet[0].value), green: CGFloat(sliderOutlet[1].value), blue: CGFloat(sliderOutlet[2].value), alpha: 1)
+        colorViewOutlet.backgroundColor = UIColor(
+            red: CGFloat(sliderOutlet[0].value),
+            green: CGFloat(sliderOutlet[1].value),
+            blue: CGFloat(sliderOutlet[2].value),
+            alpha: 1)
     }
     private func valueColorText (slider: Int){
         valueColor[slider].text = "\((round(sliderOutlet[slider].value * 100)) / 100)"
     }
-    
-    @IBAction func redSlider() {
-        valueColorText(slider: 0)
-        viewBackgroundColor()
-    }
-    @IBAction func greenSlider() {
-        valueColorText(slider: 1)
-        viewBackgroundColor()
-    }
-    @IBAction func blueSlider() {
-        valueColorText(slider: 2)
-        viewBackgroundColor()
-    }
-    
 }
 
